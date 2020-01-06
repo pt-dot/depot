fb8736e82ac564aacceeac83aae856f4f60906f6
Didik Tri Susanto <rezd14@gmail.com>
2020-01-07 02:23:56 +0700

    feature: bismillah initial commit

---
+++ source/index.blade.php
@@ -0,0 +1,51 @@
+@extends('_layouts.master')
+
+@section('body')
+<section class="container max-w-6xl mx-auto px-6 py-10 md:py-12">
+    <div class="flex flex-col-reverse mb-10 lg:flex-row lg:mb-24">
+        <div class="mt-8">
+            <h1 id="intro-docs-template">{{ $page->siteName }}</h1>
+
+            <h2 id="intro-powered-by-jigsaw" class="font-light mt-4">{{ $page->siteDescription }}</h2>
+
+            <p class="text-lg">Being Sentimental Engineer means bringing value to our customer and our engineer itself. <br class="hidden sm:block">Growth, Professional, and Don't forget to have fun.</p>
+
+            <div class="flex my-10">
+                <a href="/docs/getting-started" title="{{ $page->siteName }} getting started" class="bg-blue-500 hover:bg-blue-600 font-normal text-white hover:text-white rounded mr-4 py-2 px-6">Get Started</a>
+
+                <a href="https://dot.co.id" title="About DOT Indonesia" class="bg-gray-400 hover:bg-gray-600 text-blue-900 font-normal hover:text-white rounded py-2 px-6">About DOT Indonesia</a>
+            </div>
+        </div>
+
+        <img src="/assets/img/logo-large.svg" alt="{{ $page->siteName }} large logo" class="mx-auto mb-6 lg:mb-0 ">
+    </div>
+
+    <hr class="block my-8 border lg:hidden">
+
+    <div class="md:flex -mx-2 -mx-4">
+        <div class="mb-8 mx-3 px-2 md:w-1/3">
+            <img src="/assets/img/icon-window.svg" class="h-12 w-12" alt="window icon">
+
+            <h3 id="intro-laravel" class="text-2xl text-blue-900 mb-0">What is DEPOT</h3>
+
+            <p>DEPOT or DOT Engineering Portal is a portal that contain all engineering resources, handbook or guideline for all DOT Indonesia's Rangers. <br>Built with openess so everyone can contribute to make this site better.</p>
+        </div>
+
+        <div class="mb-8 mx-3 px-2 md:w-1/3">
+            <img src="/assets/img/icon-terminal.svg" class="h-12 w-12" alt="terminal icon">
+
+            <h3 id="intro-markdown" class="text-2xl text-blue-900 mb-0">Lorem Ipsum</h3>
+
+            <p>Lorem ipsum dolor sit amet.</p>
+        </div>
+
+        <div class="mx-3 px-2 md:w-1/3">
+            <img src="/assets/img/icon-stack.svg" class="h-12 w-12" alt="stack icon">
+
+            <h3 id="intro-mix" class="text-2xl text-blue-900 mb-0">Lorem Ipsum </h3>
+
+            <p>Lorem Ipsum Dolor Sit Amet.</p>
+        </div>
+    </div>
+</section>
+@endsection
