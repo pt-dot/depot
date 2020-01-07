0e606125dfb973a61864e03dc7e3a6c7aef4c63f
Didik Tri Susanto <rezd14@gmail.com>
2020-01-07 17:11:03 +0700

    feature: draft navigation company

--- navigation.php
+++ navigation.php
@@ -1,14 +1,17 @@
 <?php
 
 return [
-    'Getting Started' => [
+    'Company' => [
         'url' => 'docs/getting-started',
         'children' => [
-            'Customizing Your Site' => 'docs/customizing-your-site',
-            'Navigation' => 'docs/navigation',
-            'Algolia DocSearch' => 'docs/algolia-docsearch',
-            'Custom 404 Page' => 'docs/custom-404-page',
+            'Profile' => 'docs/customizing-your-site',
+            'Department & Division' => 'docs/navigation',
+            'Ranger' => 'docs/algolia-docsearch',
+            'People, Role, & Title' => 'docs/custom-404-page',
         ],
     ],
-    'Jigsaw Docs' => 'https://jigsaw.tighten.co/docs/installation',
+    'About DEPOT' => 'https://jigsaw.tighten.co/docs/installation',
+    'DOT Indonesia' => 'https://dot.co.id/',
+    'Career' => 'https://job.dot.co.id/',
+    'Internship' => 'https://internship.dot.co.id/'
 ];
