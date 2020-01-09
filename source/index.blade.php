8e16d5e2813a4ef1b6fa74f57bd450ec5b21f031
Didik Tri Susanto <rezd14@gmail.com>
2020-01-09 11:46:27 +0700

    feature: change to indonesian

--- source/index.blade.php
+++ source/index.blade.php
@@ -8,12 +8,12 @@
 
             <h2 id="intro-powered-by-jigsaw" class="font-light mt-4">{{ $page->siteDescription }}</h2>
 
-            <p class="text-lg">Being Sentimental Engineer means bringing value to our customer and our self. <br class="hidden sm:block">Growth, professional, and don't forget to have fun.</p>
+            <p class="text-lg">Tempat Sentimental Engineer DOT Indonesia dalam mencari panduan  <span class="line-through">hidup</span> <span class="italic font-semibold">"get sh*t done"</span>.</p>
 
             <div class="flex my-10">
-                <a href="/docs/company/profile" title="{{ $page->siteName }} getting started" class="bg-blue-500 hover:bg-blue-600 font-normal text-white hover:text-white rounded mr-4 py-2 px-6">Get Started</a>
+                <a href="/docs/company/profile" title="{{ $page->siteName }} getting started" class="bg-blue-500 hover:bg-blue-600 font-normal text-white hover:text-white rounded mr-4 py-2 px-6">Mulai</a>
 
-                <a href="https://dot.co.id" title="About DOT Indonesia" class="bg-gray-400 hover:bg-gray-600 text-blue-900 font-normal hover:text-white rounded py-2 px-6">About DOT Indonesia</a>
+                <a href="https://dot.co.id" title="About DOT Indonesia" class="bg-gray-400 hover:bg-gray-600 text-blue-900 font-normal hover:text-white rounded py-2 px-6">Tentang DOT Indonesia</a>
             </div>
         </div>
 
@@ -26,25 +26,25 @@
         <div class="mb-8 mx-3 px-2 md:w-1/3">
             <img src="/assets/img/icon-window.svg" class="h-12 w-12" alt="window icon">
 
-            <h3 id="intro-laravel" class="text-2xl text-blue-900 mb-0">What is DEPOT</h3>
+            <h3 id="intro-laravel" class="text-2xl text-blue-900 mb-0">Apa Itu DEPOT?</h3>
 
-            <p>DEPOT or DOT Engineering Portal is a portal that contain all engineering resources, handbook or guideline</p>
+            <p>DEPOT atau DOT Engineering Portal merupakan sebuah portal yang berisi semua sumber daya dan panduan kerja departemen engineering DOT Indonesia.</p>
         </div>
 
         <div class="mb-8 mx-3 px-2 md:w-1/3">
             <img src="/assets/img/icon-terminal.svg" class="h-12 w-12" alt="terminal icon">
 
-            <h3 id="intro-markdown" class="text-2xl text-blue-900 mb-0">Everyone Can Contribute</h3>
+            <h3 id="intro-markdown" class="text-2xl text-blue-900 mb-0">Semuanya Bisa Berkontribusi</h3>
 
-            <p>With the spirit of openess, we encourage our ranger to contribute for bringing more value to our guidelines.</p>
+            <p>Dengan semangat keterbukaan, kami menyarankan kepada DOT Indonesia ranger untuk berkontribusi untuk menjadikan panduan-panduan DEPOT lebih baik.</p>
         </div>
 
         <div class="mx-3 px-2 md:w-1/3">
             <img src="/assets/img/icon-stack.svg" class="h-12 w-12" alt="stack icon">
 
-            <h3 id="intro-mix" class="text-2xl text-blue-900 mb-0">Complete Guidelines </h3>
+            <h3 id="intro-mix" class="text-2xl text-blue-900 mb-0">Panduan Lengkap</h3>
 
-            <p>We want to provide more valuable guidelines so our Ranger won't get lost or easily adjust company's vision to our ranger.</p>
+            <p>Kami ingin menyediakan panduan yang lebih bernilai sehingga Ranger kami tidak kehilangan arah dan dapat dengan mudah menyesuaikan dengan visi perusahaan.</p>
         </div>
     </div>
 </section>
