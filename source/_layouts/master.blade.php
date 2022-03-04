2faa062a8eb1e34abeba870926453246be577edb
Nemixe <rafel.ami@gmail.com>
2022-03-04 09:04:23 +0700

    implement google analytic

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -28,7 +28,13 @@
         @stack('meta')
 
         @if ($page->production)
-            <!-- Insert analytics code here -->
+            <script async src="https://www.googletagmanager.com/gtag/js?id=G-231WEFV6GT"></script>
+            <script>
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-231WEFV6GT');
+            </script>
         @endif
 
         <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:300,300i,400,400i,700,700i,800,800i" rel="stylesheet">
