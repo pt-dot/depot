f7e4fa9224b4dd96064a71b4bd54a77a0b8f2187
Nemixe <rafel.ami@gmail.com>
2022-03-07 09:11:07 +0700

    add banner on documentation page

--- source/_layouts/documentation.blade.php
+++ source/_layouts/documentation.blade.php
@@ -5,6 +5,16 @@
 @endsection
 
 @section('body')
+<div class="relative -mt-8 mb-8 w-full">
+    <img src="/assets/images/banner.jpg" class="w-full" />
+    <div class="absolute z-10 bottom-0 w-full">
+        <div class="container max-w-8xl mx-auto px-6 md:px-8 py-5">
+            <div class="text-2xl font-bold text-white">
+                Profile
+            </div>
+        </div>
+    </div>
+</div>
 <section class="container max-w-8xl mx-auto px-6 md:px-8 py-4">
     <div class="flex flex-col lg:flex-row">
         <nav id="js-nav-menu" class="nav-menu hidden lg:block">
